795b44977e35873fb68fc8da44ed64a405244745
shetkart <shetkarthik89@gmail.com>
2022-10-01 19:49:39 +0530

    commit on 1 10 22

---
+++ C_Sharp/CustomExcep.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    public class AgeValidationError:ApplicationException
+    {
+        public AgeValidationError(string message) : base(message)
+        {
+
+        }
+    }
+    internal class CustomExcep
+    {
+       public static void Main()
+        {
+            int age;
+
+            try
+            {
+                Console.WriteLine("Enter the age of the user");
+                age = Convert.ToInt32(Console.ReadLine());
+                if(age < 22)
+                {
+                    throw (new AgeValidationError("Age is below 22"));
+                }
+                else
+                {
+                    Console.WriteLine("Age validated");
+                }
+            }
+            catch(AgeValidationError e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ C_Sharp/Excep.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Excep
+    {
+        public static void Main()
+        {
+            try
+            {
+                byte data = byte.Parse("b");
+                Console.WriteLine(data);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Provide an argument");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Entered value is not a number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Data is out of range");
+            }
+        }
+    }
+}
