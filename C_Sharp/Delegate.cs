4a8ccb4700d46c4135122027265cf067cdee3729
shetkart <shetkarthik89@gmail.com>
2022-10-03 20:02:01 +0530

    committed on 3/10

---
+++ C_Sharp/Delegate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Delegate
+    {
+        public delegate void BankDelegate(string bankname, string interest);
+        class Bank
+        {
+            public static void BankDetails(string bankname, string interest)
+            {
+                Console.WriteLine($"{bankname} and its interest is {interest}");
+            }
+            
+            public static void Main()
+            {
+                Bank b1 = new Bank();
+
+                //BankDelegate b1 = new BankDelegate(Bank.BankDetails("sbi", 2.7));
+                BankDelegate b11 = new BankDelegate(BankDetails);
+               
+                b11.Invoke("SBI", "2.70%");
+                b11("HDFC Bank", "3.00 % – 3.50 %");
+                b11("Kotak Mahindra Bank", "3.50 %");
+                b11("Axis Bank", "3.00 % – 3.50 %");
+                b11("ICICI Bank", "3.00 % – 3.50 %");
+
+            }
+        }
+
+        
+    
+        
+    
+    }
+}
---
+++ C_Sharp/Files.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    public class WriteFile
+    {
+        string path1 = "C:/Users/ei13074/Documents/Files/new.txt";
+        string path2 = "C:/Users/ei13074/Documents/Files/new2.txt";
+        string path3 = "C:/Users/ei13074/Documents/new2.txt";
+        public void Write()
+        {
+            //Specify the filename and the path of the file to be created 
+            //Fileinfo - class
+          
+
+            FileInfo fileInfo = new FileInfo(path1);
+            FileStream fileStream = fileInfo.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            StreamWriter writer = new StreamWriter(fileStream);
+            try
+            {
+                for(int i=1; i<11; i++)
+                {
+                    for(int j=1; j<11; j++)
+                    {
+                        writer.WriteLine($"{i} * {j} = {i * j}");
+                    }
+                }
+
+                //File.Move(path3, path2);
+                //File.Copy(path1, path3);
+                //File.Delete(path2);
+                      
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer.Close();
+                fileStream.Close();
+            }
+
+        }
+        public void read()
+        {
+
+            //Specify the filename and the path of the file to be created 
+            //Fileinfo - class
+            FileInfo fileInfo = new FileInfo(path1);
+            FileStream fileStream = fileInfo.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            StreamReader reader = new StreamReader(fileStream);
+            try
+            {
+                string contents = reader.ReadToEnd();
+                Console.WriteLine(contents);
+                Console.WriteLine(".................................................................");
+                string[] lines = File.ReadAllLines(path2);
+                Console.Write(lines[0]);
+                Console.WriteLine(".................................................................");
+                using (StreamReader sr = File.OpenText(path2))
+                {
+                    int count = 0;
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        count++;
+                    }
+                    Console.WriteLine(count);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+                fileStream.Close();
+            }
+
+        }
+
+    }
+    internal class Files
+    {
+      public static void Main()
+        {
+            WriteFile w = new WriteFile();
+            w.Write();
+            w.read();
+        }
+    }
+}
